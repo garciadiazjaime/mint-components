c47dacf92936aff81463b68cd62946360061ff71
Alejandro Romo <alejandro_romomartinez@intuit.com>
2020-12-22 10:14:19 -0800

    accordion work

---
+++ src/components/Accordion.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from "svelte";
+  export let data = [];
+  let component;
+
+  onMount(async () => {
+    if(theme) {
+    }
+  });
+
+</script>
+<style>
+
+</style>
+<ul>
+  {#each data as { title, content}, i}
+    <li>
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </li>
+  {/each}
+</ul>
